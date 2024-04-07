13b11a88d273145992002f52fdf8b83a8d5065b7
av7388605503 <av84002499@gmail.com>
2024-04-07 11:06:51 +0530

    update

--- src/component/Menu/Userprofile.jsx
+++ src/component/Menu/Userprofile.jsx
@@ -130,7 +130,7 @@ const Userprofile = (props) => {
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body m-auto">
-              <iframe id="qrcode" src={"https://api.mimfa.net/qrcode?value=https://new-sage-nine.vercel.app/Menu/"+encodeURIComponent(props.userLogged().userID)+"&as=value"} width="250" height="250"></iframe>
+              <iframe id="qrcode" src={"https://api.mimfa.net/qrcode?value=https://new-sage-nine.vercel.app/Menu/" + encodeURIComponent(props.userLogged().userID) + "&as=value"} width="250" height="250"></iframe>
             </div>
           </div>
         </div>
@@ -173,6 +173,11 @@ const Userprofile = (props) => {
                 <hr />
                 <form className='m-3' id='shopdtl'>
                   <label htmlFor="shopname" className="form-label ms-3">Shop Name:</label>
+                  <select id="shopname" name="shopname" className="form-select">
+                    <option value="barber shop">Barber Shop</option>
+                    <option value="fruit corner">Fruit Corner</option>
+                    <option value="hotel">Hotel</option>
+                  </select>
                   <input type="text" id="shopname" className="form-control mb-3 rounded-pill" placeholder="Shop Name" required="" />
                   <label htmlFor="address" className="form-label ms-3">address:</label>
                   <input type="text" id="address" className="form-control mb-3 rounded-pill" placeholder="address" required="" />
