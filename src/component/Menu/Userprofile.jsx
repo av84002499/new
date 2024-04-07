2c45edff2030ff0dbd6d537490aa8405ad44d3ce
av7388605503 <av84002499@gmail.com>
2024-04-07 12:19:26 +0530

    update

--- src/component/Menu/Userprofile.jsx
+++ src/component/Menu/Userprofile.jsx
@@ -181,11 +181,14 @@ const Userprofile = (props) => {
                       <label htmlFor="fcinumber" className="form-label ms-3">FCI Number</label>
                       <input type="text" id="fcinumber" className="form-control mb-3 rounded-pill" placeholder="FCI Number" required="" />
                     </div>
-                    <div className='col-sm-6'>
+                    <div className='col-sm-3'>
                       <label htmlFor="phonenumber" className="form-label ms-3">Phone Number:</label>
                       <input type="text" id="phonenumber" className="form-control mb-3 rounded-pill" placeholder="Phone Number" required="" />
                     </div>
-                   
+                    <div className='col-sm-3'>
+                      <label htmlFor="phonenumber" className="form-label ms-3">Phone Number:</label>
+                      <input type="text" id="phonenumber" className="form-control mb-3 rounded-pill" placeholder="Phone Number" required="" />
+                    </div>
                   </div>
                   <div className='row mb-3'>
                     <div className='col-sm-6'>
