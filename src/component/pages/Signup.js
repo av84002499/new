842dc3cfe7736b45ac8291f005c7e1ba47f7452d
av7388605503 <av84002499@gmail.com>
2024-04-13 10:08:33 +0530

    update

--- src/component/pages/Signup.js
+++ src/component/pages/Signup.js
@@ -49,7 +49,7 @@ const Signup = () => {
         showConfirmButton: false,
         timer: 3000, // 3 seconds
       });
-      navigate('/https://qmunuback.onrender.com/api/users/sendotp');
+      navigate('/getloginotp');
 
     } catch (error) {
       console.error('Error signing up:', error.message);
