2d7a3038e84774d01a5888b2b0db75dbd45649ab
av7388605503 <av84002499@gmail.com>
2024-04-13 07:48:06 +0530

    update

--- src/component/Menu/Qr.jsx
+++ src/component/Menu/Qr.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import QRCode from 'qrcode';
 
-function YourComponent() {
+function YourComponent(props) {
     const [imageUrl, setImageUrl] = useState('');
+    const [iframeUrl, setIframeUrl] = useState('');
 
-    
     useEffect(() => {
         const generateQRCode = async () => {
             try {
@@ -16,13 +16,19 @@ function YourComponent() {
         };
 
         generateQRCode();
+
+        // Fetch the iframe URL
+        const iframe = document.getElementById('qrcode');
+        if (iframe) {
+            setIframeUrl(iframe.src);
+        }
     }, []);
 
-    const handleDownload = () => {
+    const handleDownload = (url, fileName) => {
         // Create a temporary link element
         const link = document.createElement('a');
-        link.href = imageUrl;
-        link.download = 'qrcode.png';
+        link.href = url;
+        link.download = fileName;
         // Simulate a click on the link to trigger the download
         document.body.appendChild(link);
         link.click();
@@ -34,7 +40,18 @@ function YourComponent() {
         <div>
             {imageUrl && (
                 <div>
-                <button onClick={handleDownload}>Download QR Code</button>
+                    <img src={imageUrl} alt="QR Code" />
+                    <button onClick={() => handleDownload(imageUrl, 'qrcode.png')}>
+                        Download QR Code
+                    </button>
+                </div>
+            )}
+            {iframeUrl && (
+                <div>
+                    <iframe title='ShopQR' id="qrcode" src={iframeUrl} width="250" height="250"></iframe>
+                    <button onClick={() => handleDownload(iframeUrl, 'iframe_qrcode.png')}>
+                        Download Iframe QR Code
+                    </button>
                 </div>
             )}
         </div>
