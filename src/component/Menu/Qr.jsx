bdeeed4026d0a2db80306ef669c83704b7c0cea1
av7388605503 <av84002499@gmail.com>
2024-04-13 08:12:40 +0530

    update

--- src/component/Menu/Qr.jsx
+++ src/component/Menu/Qr.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import QRCode from 'qrcode';
 
-function YourComponent(props) {
+function YourComponent() {
     const [imageUrl, setImageUrl] = useState('');
 
+    
     useEffect(() => {
         const generateQRCode = async () => {
             try {
-                // Fetch the QR code image URL from the API
                 const response = await QRCode.toDataURL('your_text_here');
                 setImageUrl(response);
             } catch (error) {
@@ -32,12 +32,9 @@ function YourComponent(props) {
 
     return (
         <div>
-        <iframe title='ShopQR' id="qrcode" src={"https://api.mimfa.net/qrcode?value=https://new-sage-nine.vercel.app/Menu/" + encodeURIComponent(props.userLogged().userID) + "&as=value"} width="250" height="250"></iframe>
-        {imageUrl && (
+            {imageUrl && (
                 <div>
-                    <img src={imageUrl} alt="QR Code" width="250" height="250" />
-                    {/* Button to download the QR code */}
-                    <button onClick={handleDownload}>Download QR Code</button>
+                <button onClick={handleDownload}>Download QR Code</button>
                 </div>
             )}
         </div>
