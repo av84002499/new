41335ec93512a3ba57a1d4c74b42fc29836b1fab
av7388605503 <av84002499@gmail.com>
2024-04-13 07:26:55 +0530

    update

--- src/component/Menu/Qr.jsx
+++ src/component/Menu/Qr.jsx
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import QRCode from 'qrcode';
 
-function YourComponent() {
+function YourComponent(props) {
     const [imageUrl, setImageUrl] = useState('');
 
-    
     useEffect(() => {
         const generateQRCode = async () => {
             try {
-                const response = await QRCode.toDataURL('your_text_here');
+                const response = await QRCode.toDataURL('https://new-sage-nine.vercel.app/Menu/' + encodeURIComponent(props.userLogged().userID));
                 setImageUrl(response);
             } catch (error) {
                 console.log(error);
@@ -16,7 +15,7 @@ function YourComponent() {
         };
 
         generateQRCode();
-    }, []);
+    }, [props]);
 
     const handleDownload = () => {
         // Create a temporary link element
@@ -32,11 +31,20 @@ function YourComponent() {
 
     return (
         <div>
-            {imageUrl && (
-                <div>
-                <button onClick={handleDownload}>Download QR Code</button>
+            <div className="modal fade" id="qrModal" tabIndex="-1" aria-labelledby="qrModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="qrModalLabel">QR Code</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body text-center">
+                            <img src={imageUrl} alt="QR Code" />
+                            <button onClick={handleDownload}>Download QR Code</button>
+                        </div>
+                    </div>
                 </div>
-            )}
+            </div>
         </div>
     );
 }
