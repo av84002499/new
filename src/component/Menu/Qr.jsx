0254e5b1a0478a42805d2dbf7f1e77426396f4ac
av7388605503 <av84002499@gmail.com>
2024-04-14 08:31:31 +0530

    upsate

--- src/component/Menu/Qr.jsx
+++ src/component/Menu/Qr.jsx
@@ -1,22 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import QRCode from 'qrcode';
 
-function YourComponent(props) {
+function YourComponent() {
     const [imageUrl, setImageUrl] = useState('');
 
     useEffect(() => {
         const generateQRCode = async () => {
             try {
-                // Generate QR code with the provided URL
-                const response = await QRCode.toDataURL(`https://new-sage-nine.vercel.app/Menu/${encodeURIComponent(props.userLogged().userID)}`);
+                const response = await QRCode.toDataURL('your_text_here');
                 setImageUrl(response);
             } catch (error) {
                 console.log(error);
+
+                
             }
         };
 
         generateQRCode();
-    }, [props.userLogged]);
+    }, []);
 
     const handleDownload = () => {
         // Create a temporary link element
@@ -38,8 +39,6 @@ function YourComponent(props) {
                     <button onClick={handleDownload}>Download QR Code</button>
                 </div>
             )}
-            {/* Optionally include the provided iframe */}
-            <iframe title='ShopQR' id="qrcode" src={`https://api.mimfa.net/qrcode?value=https://new-sage-nine.vercel.app/Menu/${encodeURIComponent(props.userLogged().userID)}&as=value`} width="250" height="250"></iframe>
         </div>
     );
 }
