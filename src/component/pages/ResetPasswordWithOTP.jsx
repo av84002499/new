1cc2701612ba41a54bb4bad624a575df8703fdf9
av7388605503 <av84002499@gmail.com>
2024-05-05 08:51:57 +0530

    update

--- src/component/pages/ResetPasswordWithOTP.jsx
+++ src/component/pages/ResetPasswordWithOTP.jsx
@@ -19,7 +19,7 @@ const ResetPasswordWithOTP = (props) => {
         try {
             const myHeaders = new Headers();
             myHeaders.append("Content-Type", "application/json");
-            const response = await fetch('http://localhost:3200/api/users/resetPasswordWithOTP', {
+            const response = await fetch('https://qmunuback.onrender.com/api/users/resetPasswordWithOTP', {
                 method: 'POST',
                 headers: myHeaders,
                 body: JSON.stringify({ email: email, otp: otp, newPassword: password }),
