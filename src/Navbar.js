ea226cfa3b292c0dde12f3f907574a7a2388cd6c
av7388605503 <av84002499@gmail.com>
2024-06-14 15:33:07 +0530

    update

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,42 +2,35 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  // Example functions for opening and closing the navigation
-  const openNav = () => {
-    document.getElementById("mySidenav").style.width = "250px";
-  };
-
-  const closeNav = () => {
-    document.getElementById("mySidenav").style.width = "0";
+  // Handler for navigation toggle (handled through CSS class changes or custom logic as needed)
+  const toggleNav = () => {
+    const sidenav = document.getElementById("mySidenav");
+    const currentWidth = sidenav.style.width;
+    sidenav.style.width = currentWidth === '250px' ? '0' : '250px';
   };
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark py-0">
-      <div className="container-fluid mainbar">
-        <a className="navbar-brand js-scroll-trigger" href="Home">
-          <img className="nav-logo-white img-fluid" src="../images/logo-white.png" alt="logo img" />
-          <img className="nav-logo-black img-fluid" src="../images/logo-black.png" alt="logo img" />
-        </a>
-        <button className="navbar-toggler navbar-toggler-right" type="button" onClick={openNav} aria-controls="mySidenav" aria-expanded="false" aria-label="Toggle navigation">
+      <div className="container-fluid">
+        <Link className="navbar-brand js-scroll-trigger" to="/">
+          <img className="nav-logo img-fluid" src="../images/logo-white.png" alt="Logo" />
+        </Link>
+        <button className="navbar-toggler" type="button" onClick={toggleNav} aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse sidenav" id="mySidenav">
-          <ul className="navbar-nav ms-auto my-0 my-md-2 my-lg-0">
-            <li className="d-md-none d-lg-none">
-              <a href="Home" className="closebtn" onClick={closeNav}>&times;</a>
+        <div className="sidenav" id="mySidenav">
+          <ul className="navbar-nav ms-auto">
+            <li>
+              <Link className="nav-link js-scroll-trigger" to="/home" onClick={toggleNav}>Home</Link>
             </li>
-            <li className="nav-item">
-             <Link className="nav-link js-scroll-trigger" to="/home">Home</Link>
+            <li>
+              <Link className="nav-link js-scroll-trigger" to="/signin" onClick={toggleNav}>Sign in</Link>
             </li>
-            
-            <li className="nav-item">
-              <Link className="nav-link js-scroll-trigger" to="/Signin">Sign in</Link>
+            <li>
+              <Link className="nav-link js-scroll-trigger" to="/signup" onClick={toggleNav}>Sign up</Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link js-scroll-trigger" to="/Signup">Sign up</Link>
-            </li>
-            
           </ul>
+          <button className="closebtn" onClick={toggleNav}>&times;</button>
         </div>
       </div>
     </nav>
