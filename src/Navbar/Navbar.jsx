80938ec9f907402ab3acc28dd36b0c7c97d040bb
av7388605503 <av84002499@gmail.com>
2024-09-16 17:35:33 +0530

    update

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,38 +1,56 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom'; // Import useNavigate
+import { Link } from 'react-router-dom';
+
 import './Navbar.css';
 
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       window.scrollY > 50 ? setSticky(true) : setSticky(false);
     };
+
     window.addEventListener('scroll', handleScroll);
+
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
 
   return (
-    <nav className={`navbar ${sticky ? 'dark-nav' : ''}`}>
-      <img src="../images/logo1.png" alt="Logo" className="logo" />
-      <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
-        <span className={menuOpen ? 'menu-bar open' : 'menu-bar'}></span>
-        <span className={menuOpen ? 'menu-bar open' : 'menu-bar'}></span>
-        <span className={menuOpen ? 'menu-bar open' : 'menu-bar'}></span>
+    <nav className={`navbar-container ${sticky ? 'dark-nav' : ''}`}>
+      <img src="../images/logo-white.png" alt="Logo" className="logo" />
+      <div className={`menu-icon ${mobileMenuOpen ? 'open' : ''}`} onClick={toggleMobileMenu}>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
       </div>
-      <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
-        <li>
-          <Link className="nav-link" to="/signin" onClick={() => setMenuOpen(false)}>Sign in</Link>
+      <ul className={`nav-links ${mobileMenuOpen ? 'open' : ''}`}>
+        <li><Link to="/">Home</Link></li>
+        {/* Account Dropdown */}
+        <li className="account-dropdown" onClick={toggleDropdown}>
+          <span className="dropdown-toggle">Account</span>
+          <ul className={`dropdown-menu ${dropdownOpen ? 'open' : ''}`}>
+            <li><Link to="/signin">Signin</Link></li>
+            <li><Link to="/signup">Signup</Link></li>
+          </ul>
         </li>
-        <li>
-          <Link className="nav-link" to="/signup" onClick={() => setMenuOpen(false)}>Sign up</Link>
-        </li>
-       
+        <li><Link to="/about">About us</Link></li>
+        <li><Link to="/contact">Contact us</Link></li>
+        <li><Link to="/Qrcode">Qrcode</Link></li>
+        
+        
       </ul>
     </nav>
   );
